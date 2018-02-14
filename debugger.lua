d858a119b6b0cb3a021e79ec06abe10e91baf39e
aleclarson <alec.stanford.larson@gmail.com>
2018-02-14 11:28:16 -0500

    fix whitespace

--- debugger.lua
+++ debugger.lua
@@ -1,16 +1,16 @@
 --[[
 	Copyright (c) 2016 Scott Lembcke and Howling Moon Software
-	
+
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
-	
+
 	The above copyright notice and this permission notice shall be included in
 	all copies or substantial portions of the Software.
-	
+
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,7 +18,7 @@
 	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 	SOFTWARE.
-	
+
 	TODO:
 	* Print short function arguments as part of stack location.
 	* Bug: sometimes doesn't advance to next line (same line event reported multiple times).
@@ -37,18 +37,18 @@ local function pretty(obj, recurse)
 		local meta = getmetatable(tbl)
 		return (meta and meta.__tostring)
 	end
-	
+
 	if type(obj) == "string" then
 		-- Dump the string so that escape sequences are printed.
 		return string.format("%q", obj)
 	elseif type(obj) == "table" and not coerceable(obj) and not recurse then
 		local str = "{"
-		
+
 		for k, v in pairs(obj) do
 			local pair = pretty(k, true).." = "..pretty(v, true)
 			str = str..(str == "{" and pair or ", "..pair)
 		end
-		
+
 		return str.."}"
 	else
 		-- tostring() can fail if there is an error in a __tostring metamethod.
@@ -116,7 +116,7 @@ local function hook_factory(repl_threshold)
 	return function(offset)
 		return function(event, _)
 			local info = debug.getinfo(2)
-			
+
 			if event == "call" and info.linedefined >= 0 then
 				offset = offset + 1
 			elseif event == "return" and info.linedefined >= 0 then
@@ -142,7 +142,7 @@ local function table_merge(t1, t2)
 	local tbl = {}
 	for k, v in pairs(t1) do tbl[k] = v end
 	for k, v in pairs(t2) do tbl[k] = v end
-	
+
 	return tbl
 end
 
@@ -152,21 +152,21 @@ local function local_bindings(offset, include_globals)
 	local level = stack_offset + offset + LOCAL_STACK_LEVEL
 	local func = debug.getinfo(level).func
 	local bindings = {}
-	
+
 	-- Retrieve the upvalues
 	do local i = 1; repeat
 		local name, value = debug.getupvalue(func, i)
 		if name then bindings[name] = value end
 		i = i + 1
 	until name == nil end
-	
+
 	-- Retrieve the locals (overwriting any upvalues)
 	do local i = 1; repeat
 		local name, value = debug.getlocal(level, i)
 		if name then bindings[name] = value end
 		i = i + 1
 	until name == nil end
-	
+
 	-- Retrieve the varargs. (works in Lua 5.2 and LuaJIT)
 	local varargs = {}
 	do local i = -1; repeat
@@ -175,12 +175,12 @@ local function local_bindings(offset, include_globals)
 		i = i - 1
 	until name == nil end
 	if #varargs ~= 0 then bindings["..."] = varargs end
-	
+
 	if include_globals then
 		-- Merge the local bindings over the top of the environment table.
 		-- In Lua 5.2, you have to get the environment table from the function's locals.
 		local env = (_VERSION <= "Lua 5.1" and getfenv(func) or bindings._ENV)
-		
+
 		-- Finally, merge the tables and add a lookup for globals.
 		return setmetatable(table_merge(env or {}, bindings), {__index = _G})
 	else
@@ -191,7 +191,7 @@ end
 -- Compile an expression with the given variable bindings.
 local function compile_chunk(expr, env)
 	local source = "debugger.lua REPL"
-	
+
 	if _VERSION <= "Lua 5.1" then
 		local chunk = loadstring("return "..expr, source)
 		if chunk then setfenv(chunk, env) end
@@ -212,10 +212,10 @@ local function cmd_print(expr)
 		dbg.writeln(COLOR_RED.."Error: Could not evaluate expression."..COLOR_RESET)
 		return false
 	end
-	
+
 	-- Call the chunk and collect the results.
 	local results = {pcall(chunk, unpack(rawget(env, "...") or {}))}
-	
+
 	-- The first result is the pcall error.
 	if not results[1] then
 		dbg.writeln(COLOR_RED.."Error:"..COLOR_RESET.." %s", results[2])
@@ -226,22 +226,22 @@ local function cmd_print(expr)
 		for i = 2, #results do
 			result = result..(i ~= 2 and ", " or "")..pretty(results[i])
 		end
-		
+
 		dbg.writeln(COLOR_BLUE..expr..COLOR_RED.." => "..COLOR_RESET..result)
 	end
-	
+
 	return false
 end
 
 local function cmd_up()
 	local info = debug.getinfo(stack_offset + LOCAL_STACK_LEVEL + 1)
-	
+
 	if info then
 		stack_offset = stack_offset + 1
 	else
 		dbg.writeln(COLOR_BLUE.."Already at the top of the stack."..COLOR_RESET)
 	end
-	
+
 	dbg.writeln("Inspecting frame: "..format_stack_frame_info(debug.getinfo(stack_offset + LOCAL_STACK_LEVEL)))
 	return false
 end
@@ -252,7 +252,7 @@ local function cmd_down()
 	else
 		dbg.writeln(COLOR_BLUE.."Already at the bottom of the stack."..COLOR_RESET)
 	end
-	
+
 	dbg.writeln("Inspecting frame: "..format_stack_frame_info(debug.getinfo(stack_offset + LOCAL_STACK_LEVEL)))
 	return false
 end
@@ -262,38 +262,38 @@ local function cmd_trace()
 	local offset = stack_offset - stack_top
 	local message = string.format("Inspecting frame: %d - (%s)", offset, location)
 	local str = debug.traceback(message, stack_top + LOCAL_STACK_LEVEL)
-	
+
 	-- Iterate the lines of the stack trace so we can highlight the current one.
 	local line_num = -2
 	while str and #str ~= 0 do
 		local line, rest = string.match(str, "([^\n]*)\n?(.*)")
 		str = rest
-		
+
 		if line_num >= 0 then line = tostring(line_num)..line end
 		dbg.writeln((line_num + stack_top == stack_offset) and COLOR_BLUE..line..COLOR_RESET or line)
 		line_num = line_num + 1
 	end
-	
+
 	return false
 end
 
 local function cmd_locals()
 	local bindings = local_bindings(1, false)
-	
+
 	-- Get all the variable binding names and sort them
 	local keys = {}
 	for k, _ in pairs(bindings) do table.insert(keys, k) end
 	table.sort(keys)
-	
+
 	for _, k in ipairs(keys) do
 		local v = bindings[k]
-		
+
 		-- Skip the debugger object itself, temporaries and Lua 5.2's _ENV object.
 		if not rawequal(v, dbg) and k ~= "_ENV" and k ~= "(*temporary)" then
 			dbg.writeln("\t"..COLOR_BLUE.."%s "..COLOR_RED.."=>"..COLOR_RESET.." %s", k, pretty(v))
 		end
 	end
-	
+
 	return false
 end
 
@@ -313,7 +313,7 @@ local function match_command(line)
 		["h"] = function() dbg.writeln(help_message); return false end,
 		["q"] = function() os.exit(0) end,
 	}
-	
+
 	for cmd, cmd_func in pairs(commands) do
 		local matches = {string.match(line, "^("..cmd..")$")}
 		if matches[1] then
@@ -330,14 +330,14 @@ local function run_command(line)
 		dbg.writeln()
 		return true
 	end
-	
+
 	-- Re-execute the last command if you press return.
 	if line == "" then
 		if last_cmd then line = last_cmd else return false end
 	else
 		last_cmd = line
 	end
-	
+
 	local command, command_arg = match_command(line)
 	if command then
 		-- unpack({...}) prevents tail call elimination so the stack frame indices are predictable.
@@ -350,7 +350,7 @@ end
 
 repl = function()
 	dbg.writeln(format_stack_frame_info(debug.getinfo(LOCAL_STACK_LEVEL - 3 + stack_top)))
-	
+
 	repeat
 		local success, done, hook = pcall(run_command, dbg.read(COLOR_RED.."debugger.lua> "..COLOR_RESET))
 		if success then
@@ -367,11 +367,11 @@ end
 dbg = setmetatable({}, {
 	__call = function(self, condition, offset)
 		if condition then return end
-		
+
 		offset = (offset or 0)
 		stack_offset = offset
 		stack_top = offset
-		
+
 		debug.sethook(hook_next(1), "crl")
 		return
 	end,
@@ -388,7 +388,7 @@ function dbg.error(err, level)
 	level = level or 1
 	dbg.writeln(COLOR_RED.."Debugger stopped on error:"..COLOR_RESET.."(%s)", pretty(err))
 	dbg(false, level)
-	
+
 	error(err, level)
 end
 
@@ -398,7 +398,7 @@ function dbg.assert(condition, message)
 		dbg.writeln(COLOR_RED.."Debugger stopped on "..COLOR_RESET.."assert(..., %s)", message)
 		dbg(false, 1)
 	end
-	
+
 	assert(condition, message)
 end
 
@@ -407,7 +407,7 @@ function dbg.call(f, l)
 	return (xpcall(f, function(err)
 		dbg.writeln(COLOR_RED.."Debugger stopped on error: "..COLOR_RESET..pretty(err))
 		dbg(false, (l or 0) + 1)
-		
+
 		-- Prevent a tail call to dbg().
 		return
 	end))
@@ -417,7 +417,7 @@ end
 function dbg.msgh(...)
 	dbg.write(...)
 	dbg(false, 1)
-	
+
 	return ...
 end
 
@@ -441,11 +441,11 @@ if ffi then
 	ffi.cdef[[
 		bool isatty(int);
 		void free(void *ptr);
-		
+
 		char *readline(const char *);
 		int add_history(const char *);
 	]]
-	
+
 	stdin_isatty = ffi.C.isatty(0)
 	stdout_isatty = ffi.C.isatty(1)
 end
@@ -463,7 +463,7 @@ local dbg_readline = nil
 pcall(function()
 	if ffi and stdin_isatty and not os.getenv("DBG_NOREADLINE") then
 		local readline = ffi.load("readline")
-		
+
 		dbg_readline = function(prompt)
 			local cstr = readline.readline(prompt)
 			if cstr ~= nil then
@@ -471,14 +471,14 @@ pcall(function()
 				if string.match(str, "[^%s]+") then
 					readline.add_history(cstr)
 				end
-				
+
 				ffi.C.free(cstr)
 				return str
 			else
 				return nil
 			end
 		end
-		
+
 		dbg.read = dbg_readline
 		dbg.writeln(COLOR_RED.."debugger.lua: Readline support enabled."..COLOR_RESET)
 	end
